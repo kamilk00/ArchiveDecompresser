9bec2171aa3e3883fab6fd990d7dbddf7903f938
kamilk00 <kkaczmarek631@gmail.com>
2023-08-02 09:16:22 +0200

    Compress to only one button for all of archive file types.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,47 +29,30 @@ namespace ArchiveDecompresser
             InitializeComponent();
         }
 
-
-        private void buttonZip_Click(object sender, RoutedEventArgs e)
+        private void buttonDecompress_Click(object sender, RoutedEventArgs e)
         {
-            OpenExplorer(".zip");
+            OpenExplorer();
         }
 
-        private void button7z_Click(object sender, RoutedEventArgs e)
-        {
-            OpenExplorer(".7z");
-        }
-
-        private void buttonTar_Click(object sender, RoutedEventArgs e)
-        {
-            OpenExplorer(".tar");
-        }
-
-        private void buttonRar_Click(object sender, RoutedEventArgs e)
-        {
-            OpenExplorer(".rar");
-        }
-
-        private void OpenExplorer(string extension)
+        private void OpenExplorer()
         {
 
             //open the file explorer window to select the compressed file
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Compressed Files (*.zip;*.7z;*.rar)|*.zip;*.7z;*.rar";
-            openFileDialog.Filter = extension + " Files (*" + extension + ")|*" + extension;
+            openFileDialog.Filter = "Compressed Files (*.zip;*.7z;*.rar;*.tar)|*.zip;*.7z;*.rar;*.tar";
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             if (openFileDialog.ShowDialog() == true)
             {
 
                 //determine the file format and call the decompression method
                 string compressedFilePath = openFileDialog.FileName;
-                DecompressFile(compressedFilePath, extension);
+                DecompressFile(compressedFilePath);
 
             }
 
         }
 
-        private void DecompressFile(string compressedFilePath, string extension)
+        private void DecompressFile(string compressedFilePath)
         {
 
             string extractionPath = System.IO.Path.GetDirectoryName(compressedFilePath);
@@ -91,11 +74,11 @@ namespace ArchiveDecompresser
                             });
                         }
                     }
-                    MessageBox.Show(extension + " file decompressed successfully!");
+                    MessageBox.Show("File decompressed successfully!");
                 }
                 catch
                 {
-                    MessageBox.Show("Error during decompression " + extension + " file!");
+                    MessageBox.Show("Error during file decompression!");
                 }
 
             }
